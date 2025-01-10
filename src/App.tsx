e0cdb9d36c2c353d3b9d0071994f0ab84984eb5b
opteron-x86 <opteronx86@protonmail.com>
2025-01-10 12:00:16 -0500

    initial deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import { useAuthenticator } from '@aws-amplify/ui-react';
 
 const client = generateClient<Schema>();
 
 function App() {
+  const { user, signOut } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+    
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -19,11 +25,11 @@ function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li onClick={() => deleteTodo(todo.id)} key={todo.id}>{todo.content}</li>
         ))}
       </ul>
       <div>
@@ -33,6 +39,7 @@ function App() {
           Review next step of this tutorial.
         </a>
       </div>
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
